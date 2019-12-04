6a53d80092bb5fb811086c80fa0938b345700d5c
Osmar da Cunha Filho <osmarcf@gmail.com>
2019-12-04 11:39:29 -0300

    todas as operacoes com o spring boot

--- agenda/src/main/java/br/edu/usj/ads/lpii/agenda/ContatosController.java
+++ agenda/src/main/java/br/edu/usj/ads/lpii/agenda/ContatosController.java
@@ -16,7 +16,7 @@ public class ContatosController {
     @Autowired
     ContatoRepository contatoRepository;
 
-    @GetMapping
+    @RequestMapping(method = RequestMethod.GET)
     public ModelAndView listar() {
         List<Contato> lista = new ArrayList<>();
         contatoRepository.findAll().forEach(lista::add);
@@ -27,28 +27,77 @@ public class ContatosController {
         return modelAndView;
     }
 
-    @PostMapping
+    @RequestMapping(path = "/{id}", method = RequestMethod.GET)
+    public ModelAndView pesquisarPorId(@PathVariable Long id) {
+        ModelAndView modelAndView = new ModelAndView("mostraContato");
+
+        if (contatoRepository.findById(id).isPresent()) {
+            modelAndView.addObject("contato", contatoRepository.findById(id).get());
+        }
+
+        return modelAndView;
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
     public String adiciona(@RequestParam String nome, @RequestParam String cidade)
     {
         Contato c = new Contato();
         c.setNome(nome);
         c.setCidade(cidade);
 
-        contatoRepository.save(c);
+        Contato contatoSalvo = contatoRepository.save(c);
 
-        return "listaContatos";
+        return "redirect:/contato/" + contatoSalvo.getId();
     }
 
-    @RequestMapping(path="/delete/{id}", method = RequestMethod.GET)
+    @RequestMapping(path="/{id}/delete", method = RequestMethod.GET)
     public String apaga(@PathVariable Long id) {
         contatoRepository.deleteById(id);
-        return "listaContatos";
+        return "redirect:/contato";
     }
 
     @RequestMapping(path="/delete", method = RequestMethod.POST)
-    public String apagaForm(@RequestParam Long id) {
+    public ModelAndView apagaDadosForm(@RequestParam Long id) {
         contatoRepository.deleteById(id);
-        return "listaContatos";
+        ModelAndView modelAndView = new ModelAndView("redirect:/contato");
+        return modelAndView;
+    }
+
+    @RequestMapping(path="/deleteCombo", method = RequestMethod.GET)
+    public ModelAndView apagaMostraFormCombo() {
+        List<Contato> lista = new ArrayList<>();
+        contatoRepository.findAll().forEach(lista::add);
+
+        ModelAndView modelAndView = new ModelAndView("apagaContatoCombo");
+        modelAndView.addObject("contatos", lista);
+
+        return modelAndView;
+    }
+
+
+    @RequestMapping(path = "/{id}/update", method = RequestMethod.GET)
+    public ModelAndView editarMostraForm(@PathVariable Long id) {
+        ModelAndView modelAndView = new ModelAndView("editaContato");
+
+        if (contatoRepository.findById(id).isPresent()) {
+            modelAndView.addObject("contato", contatoRepository.findById(id).get());
+        }
+
+        return modelAndView;
+    }
+
+    @RequestMapping(path = "/update", method = RequestMethod.POST)
+    public ModelAndView editar(@RequestParam Long id, @RequestParam String nome, @RequestParam String cidade) {
+        Contato c = new Contato();
+        c.setId(id);
+        c.setNome(nome);
+        c.setCidade(cidade);
+
+        Contato contatoSalvo = contatoRepository.save(c);
+
+        ModelAndView modelAndView = new ModelAndView("redirect:/contato/" + contatoSalvo.getId());
+
+        return modelAndView;
     }
 
 }
